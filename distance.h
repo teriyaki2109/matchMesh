c5c625ae218f403f01ba5c4d08d98071877e68d3
Tony TUNG <tony2ng@gmail.com>
2014-06-05 11:12:06 +0900

    first commit

---
+++ distance.h
@@ -0,0 +1,10 @@
+#include <math.h>
+
+float L1(float *histo1, float *histo2, int size);
+float L2(float *histo1, float *histo2, int size);
+float Linf(float *histo1, float *histo2, int size);
+float Battacharyya(float *histo1, float *histo2, int size);
+float Jeffreys(float *histo1, float *histo2, int size);
+float ChiSquared(float *histo1, float *histo2, int size);
+float HistoIntersection(float *histo1, float *histo2, int size);
+
